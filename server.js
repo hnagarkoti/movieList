2f7795b7abf7bfee6ee649002b8e9592da9b12b6
hnagarkoti <hemantnagarkoti5@gmail.com>
2016-12-16 11:38:18 +0530

    test openshift

--- server.js
+++ server.js
@@ -24,15 +24,6 @@ if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
   process.env.OPENSHIFT_APP_NAME;
 }
 
-mongoose.connect('mongodb://'+connection_string, function(err){
-  if(err)
-  console.log('error while conneting mongo:- ',err);
-  else {
-    console.log('mongo connected');
-  }
-})
-
-console.log(connection_string);
 var routes = require('./routes/index');
 // var users = require('./routes/users');
 
@@ -91,12 +82,23 @@ app.use(function(err, req, res, next) {
   });
 });
 
+
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
 app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 
 app.listen(app.get('port') ,app.get('ip'), function () {
     console.log("✔ Express server listening at %s:%d ", app.get('ip'),app.get('port')); // start the server and listen to port we set in config.js
 });
+
+mongoose.connect('mongodb://'+connection_string, function(err){
+  if(err)
+  console.log('error while conneting mongo:- ',err);
+  else {
+    console.log('mongo connected');
+  }
+})
+
+console.log(connection_string);
 
 // app.set('port', 8080);
 // app.listen(app.get('port'));
