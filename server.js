e9ce5c7512cc7021a969ee46c65ac8cfc1de77a0
hnagarkoti <hemantnagarkoti5@gmail.com>
2016-12-16 10:52:45 +0530

    port changed according to openshift

--- server.js
+++ server.js
@@ -71,7 +71,14 @@ app.use(function(err, req, res, next) {
   });
 });
 
-app.set('port', 8080);
-app.listen(app.get('port'));
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
+
+app.listen(app.get('port') ,app.get('ip'), function () {
+    console.log("✔ Express server listening at %s:%d ", app.get('ip'),app.get('port')); // start the server and listen to port we set in config.js
+});
+
+// app.set('port', 8080);
+// app.listen(app.get('port'));
 
 module.exports = app;
